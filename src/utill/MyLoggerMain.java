22931fc792ba70731cb9833e6c54f460f244b519
Mincheol-Ha <hmc0501@gmail.com>
2025-03-29 11:47:46 +0900

    Logger

---
+++ src/utill/MyLoggerMain.java
@@ -0,0 +1,11 @@
+package utill;
+
+import static utill.MyLogger.*;
+
+public class MyLoggerMain {
+
+    public static void main(String[] args) {
+        log("hello thread");
+        log(123);
+    }
+}
